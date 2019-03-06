2b59551b149dc3cabd3ba7a7f573c803bf581684
Vanessa Wang <sanguliupo@gmail.com>
2019-03-05 17:19:59 -0800

    linkedlist reverse function

---
+++ singly_linkedlists_reverse.js
@@ -0,0 +1,114 @@
+// 1->10->99->5->16
+class Node{
+  constructor(value){
+    this.value=value;
+    this.next=null;
+  }
+}
+
+class LinkedList{
+  constructor(value){
+    this.head={
+      value: value,
+      next:null
+    }
+  
+  this.tail=this.head;
+  this.length=1;
+  }
+  
+  append(value){
+    const newNode= new Node(value);
+    this.tail.next=newNode;
+    this.tail=newNode;
+    this.length++;
+  }
+
+
+  prepend(value){
+    const newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  printList(){
+    const array=[];
+    let currentNode = this.head;
+    while (currentNode !== null){
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value){
+    //check params
+    if (index>this.length){
+      this.append(value);
+    }
+
+    const newNode = new Node(value);
+    if (index<1){
+      this.prepend(value)
+    }else{
+      const leader = this.traverseToIndex(index-1);
+      const holdingPointer = leader.next;
+      leader.next = newNode;
+      newNode.next = holdingPointer;
+    }
+    this.length++;
+  }
+
+  remove(index){
+    if (index>this.length){
+      return this;
+    }
+    if (index<1){
+      this.head = this.head.next;
+    }else{
+      const leader = this.traverseToIndex(index-1);
+      leader.next = leader.next.next;
+    }
+    this.length--;
+  }
+  
+
+  traverseToIndex(index){
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index){
+      currentNode = currentNode.next;
+      counter++;
+    } 
+    return currentNode;
+  }
+
+  reverse(){
+    if (!this.head.next){
+      return this;
+    }
+    let first = this.head;
+    this.tail=this.head;
+    let second = first.next;
+    while (second){
+      const temp = second.next;
+      second.next = first;
+      first = second; 
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+  }
+}
+
+
+const myLinkedList= new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2,77);
+myLinkedList.remove(2);
+myLinkedList.reverse();
+console.log(myLinkedList.printList());
+// console.log(myLinkedList);
