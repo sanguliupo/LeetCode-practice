e8be8f0a4fba14b3a19daa8f6c05e9624380f3fd
Vanessa Wang <sanguliupo@gmail.com>
2019-03-18 14:39:43 -0700

    better solution for counting prime numbers

---
+++ countPrime_2.js
@@ -0,0 +1,19 @@
+var countPrimes = function(n) {
+    let flagArray = [],
+        result = 0;
+    for(let i = 2; i < n; i++){
+        if(flagArray[i] === undefined){
+            flagArray[i] = 1; //i is a prime number
+            result++;
+            // rm its multiples
+            let j = 2;
+            while(i * j < n){
+                flagArray[i * j] = 0; //i*j is not a Prime number
+                j++;
+            }
+        }
+    }
+    return result;
+}
+
+console.log(countPrimes(15));
